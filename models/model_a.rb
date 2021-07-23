688d39446eb2920a3c9d043fcb5e57ae8d9c1597
Mallory Vining <mallorygallery12@gmail.com>
2021-07-23 14:42:23 -0600

    Add hash model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+SCHOOL = {
+  "name" => "Turing",
+  "location" => "Denver",
+  "number_of_graduates" => 1200,
+  "number_of_mod0_students" => 44,
+  "is_difficult" => true,
+  "is_valuable" => true,
+  "successful_qualities" => ["focused", "mindful", "collaborative"],
+  "unsuccessful_qualities" => ["combative", "inattentive", "unwilling to ask for help"]
+}

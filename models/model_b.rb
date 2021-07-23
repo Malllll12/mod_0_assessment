ce458dd184a1c5b7311cd16cadc9450acc82b543
Mallory Vining <mallorygallery12@gmail.com>
2021-07-23 14:48:01 -0600

    Add pet traits

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+PET = {
+  "name" => "Sgt Pepper",
+  "breed" => "basset hound",
+  "age" => 1,
+  "yearly_cost_in_USD" => 1000,
+  "is_sweet" => true,
+  "is_active" => false,
+  "color" => ["red", "white", "black"],
+  "qualities" => ["easy-going", "friendly", "cuddly"]
+}

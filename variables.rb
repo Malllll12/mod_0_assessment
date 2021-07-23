66502f11a11d2f63ca516cf8e6e755298bc54371
Mallory Vining <mallorygallery12@gmail.com>
2021-07-23 14:28:42 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Simone Biles"
+is_active = true
+famous_person = "Britney Spears"
+num_3 = 12
+num_9 = 9
+sum = num_3 + num_9
+animals = ["cheetah", "lemur", "hare"]
+animals.shift
+famous_person.length
